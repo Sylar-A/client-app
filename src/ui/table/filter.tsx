e55e826a818f77add0f09c957aa046572319cec5
Azat Shaydullin <a.shaidullin@bars.group>
2024-03-17 21:29:44 +0300

    Исправлена ошибка постоянного перерендеринга

--- src/ui/table/filter.tsx
+++ src/ui/table/filter.tsx
@@ -1,13 +1,14 @@
 import { Column } from "@tanstack/react-table";
-import { DebouncedInput } from "./debouncedInput";
+import { Input } from "../input";
 
 export function Filter({ column }: { column: Column<any, unknown> }) {
 	return (
 		<div className="flex justify-center">
-			<DebouncedInput
-				type="text"
+			<Input
 				value={(column.getFilterValue() ?? "") as string | number}
-				onChange={(value) => column.setFilterValue(value)}
+				onChange={(event) => {
+					column.setFilterValue(event.target.value);
+				}}
 				className="bg-search dark:bg-white-search focus:bg-none bg-no-repeat bg-center h-6"
 			/>
 		</div>
